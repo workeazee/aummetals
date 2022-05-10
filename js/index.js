53bc40bc786b3cd2bf53a9b9f6c0d08c5010e6c0
Khan047 <tajzz.tk@gmail.com>
2022-05-10 22:20:15 +0530

    complete slider styles :zap:

--- js/index.js
+++ js/index.js
@@ -4,24 +4,44 @@
 //   alert('loaded')
 
 $(document).ready(function(){
-  $('.your-class').slick({
-    infinite: true,
-    dots: true,
-    prevArrow:"<button type='button' class='slick-prev pull-left'><div class='arrow-wrapper'><div class='icon-left'><svg viewBox='0 0 24 24' width='30px' height='30px' focusable='false'><path fill='rgba(255,255,255,1)' d='M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z'></path><path d='M0-.75h24v24H0z' fill='none'></path></svg></div></div></button>",
-    nextArrow:"<button type='button' class='slick-next pull-right'><div class='arrow-wrapper'><div class='icon-right'><svg viewBox='0 0 24 24' width='30px' height='30px' focusable='false'><path fill='rgba(255,255,255,1)' d='M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z'></path><path d='M0-.75h24v24H0z' fill='none'></path></svg></div></div></button>"
-  });
-  console.log('test');
-  $( "#sl1" ).mouseover(function() {
-    console.log('ds')
-    $( " .arrow-wrapper",this ).css( 'display','flex' );
-  });
-  $( "#sl1" ).mouseleave(function() {
-    console.log('ds')
-    $( " .arrow-wrapper",this ).css( 'display','none' );
-  });
-  $(".your-class").on("onmouseover", function () {
-    console.log('dsada')
-});
+  // $('#sl1').slick({
+  //   infinite: true,
+  //   dots: true,
+  //   autoplay: true,
+  //   prevArrow:"<button type='button' class='slick-prev pull-left'><div class='arrow-wrapper'><div class='icon-left'><svg viewBox='0 0 24 24' width='30px' height='30px' focusable='false'><path fill='rgba(255,255,255,1)' d='M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z'></path><path d='M0-.75h24v24H0z' fill='none'></path></svg></div></div></button>",
+  //   nextArrow:"<button type='button' class='slick-next pull-right'><div class='arrow-wrapper'><div class='icon-right'><svg viewBox='0 0 24 24' width='30px' height='30px' focusable='false'><path fill='rgba(255,255,255,1)' d='M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z'></path><path d='M0-.75h24v24H0z' fill='none'></path></svg></div></div></button>"
+  // });
+  // console.log('test');
+  // $( "#sl1" ).mouseover(function() {
+  //   console.log('ds')
+  //   $( " .arrow-wrapper",this ).css( 'display','flex' );
+  // });
+  // $( "#sl1" ).mouseleave(function() {
+  //   console.log('ds')
+  //   $( " .arrow-wrapper",this ).css( 'display','none' );
+  // });
+
+  for (let i = 1; i <= 5; i++) {
+    const element = 5
+    console.log(`#sl${i}`)
+    $(`#sl${i}`).slick({
+      infinite: true,
+      dots: true,
+      autoplay: true,
+      prevArrow:"<button type='button' class='slick-prev pull-left'><div class='arrow-wrapper'><div class='icon-left'><svg viewBox='0 0 24 24' width='30px' height='30px' focusable='false'><path fill='rgba(255,255,255,1)' d='M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z'></path><path d='M0-.75h24v24H0z' fill='none'></path></svg></div></div></button>",
+      nextArrow:"<button type='button' class='slick-next pull-right'><div class='arrow-wrapper'><div class='icon-right'><svg viewBox='0 0 24 24' width='30px' height='30px' focusable='false'><path fill='rgba(255,255,255,1)' d='M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z'></path><path d='M0-.75h24v24H0z' fill='none'></path></svg></div></div></button>"
+    });
+    console.log('test');
+    $( "#sl"+i ).mouseover(function() {
+      console.log('ds')
+      $( " .arrow-wrapper",this ).css( 'display','flex' );
+    });
+    $( "#sl"+i ).mouseleave(function() {
+      console.log('ds')
+      $( " .arrow-wrapper",this ).css( 'display','none' );
+    });
+  }
+
  })();
  })();
 
